ee726e4d42657d58518b4b3d18f85877ef7b4616
Nikolay Amiantov <ab@fmap.me>
2015-08-08 01:47:52 +0300

    yet more scripts

--- broadcast_string.rb
+++ broadcast_string.rb
@@ -3,6 +3,8 @@
 require 'net/http'
 require 'json'
 
+$phrase = "ph'nglui mglw'nafh cthulhu r'lyeh wgah'nagl fhtagn!"
+#$phrase = "ei! 000k4k4ei! 22224ei! 24ei!044ei!004222ei!004224222240044"
 $token = "53/b8w5nkWTgqhWm00puFJMoBk3NPMMs3TAPAD8eSU0="
 $team = 180
 
@@ -10,9 +12,9 @@ uri = URI("https://davar.icfpcontest.org/teams/#{$team}/solutions")
 req = Net::HTTP::Post.new uri
 req.basic_auth '', $token
 req['Content-Type'] = 'application/json'
-data = File.read(ARGF.argv[0])
 
 Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
+  data = (0..23).map { |i| { problemId: i, seed: 0, tag: "H", solution: $phrase } }
   req.body = data.to_json
   http.request req
 end
