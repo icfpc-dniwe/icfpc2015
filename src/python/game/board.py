dcfaae2daba5fc5bb040b2caa82efac06be903a8
vuvko <notvuvko@gmail.com>
2015-08-08 20:41:54 +0300

    Board bounds and lock checks.

--- src/python/game/board.py
+++ src/python/game/board.py
@@ -10,14 +10,23 @@ class Board:
     def __init__(self, width, height):
         self.width = width
         self.height = height
-        self.filled = []
+        self.fills
         #self._rows = {}
+    
+    
+    def __check_bounds__(self, points):
+      return (any(points[:, 2] < 0) or \ # top
+              any(points[:, 0] >= self.width - (points[:, 2] // 2)) or \ # right x
+              any(points[:, 0] >= - self.width - ((points[:, 2] + 1) // 2)) or \ # right y
+              any(points[:, 2] >= height) or \ # bottom
+              any(points[:, 0] < -(points[:, 2] // 2)) or \ # left x
+              any(points[:, 0] < -((points[:, 2] + 1) // 2)) # left y
 
 
     def add_cells(self, points):
         points = point2hex(points)
 
 
-    def is_filled(self, points):
-        pass
-
+    def is_locked(self, points):
+        return __check_bounds__(self, points) or \
+          any([(points == cell).any() for cell in self.filled])
