f5578c50756179dec11aa9e2f96026496717d45e
vuvko <notvuvko@gmail.com>
2015-08-08 19:26:29 +0300

    Can now transform back to 2D points.

--- src/python/common/tools.py
+++ src/python/common/tools.py
@@ -17,8 +17,15 @@ def nvl2(val, res1, res2):
 
 def points2hex(points):
     points2d = np.array(points, dtype='int32')
-    p_hex = np.zeros((points2d.shape[0], 3), dtype='int32').squeeze()
+    p_hex = np.zeros((len(points), 3), dtype='int32').squeeze()
     p_hex[:, 0] = points2d[:, 0] - (points2d[:, 1] - (points2d[:, 1] & 1)) // 2
     p_hex[:, 2] = points2d[:, 1]
     p_hex[:, 1] = -p_hex[:, 0] - p_hex[:, 1]
     return p_hex
+
+
+def hex2points(p_hex):
+    points = np.zeros((len(p_hex), 2), dtype='int32')
+    points[:, 0] = p_hex[:, 0] + (p_hex[:, 2] - (p_hex[:, 2] & 1)) // 2
+    points[:, 1] = p_hex[:, 2]
+    return points
