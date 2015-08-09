5a9cabc65b86e06485394f002646850838317a00
alvearian <am-vi@yandex.ru>
2015-08-09 15:00:33 +0300

    find all final states

--- src/python/game/board.py
+++ src/python/game/board.py
@@ -15,7 +15,7 @@ from common.constants import Move, Rotate
 
 from copy import deepcopy
 
-POSSIBLE_MOVES = set([Move.W, Move.E, Move.SW, Move.SE, Rotate.CW, Rotate.CCW])
+POSSIBLE_MOVES = set([Move.E, Move.W, Move.SW, Move.SE, Rotate.CW, Rotate.CCW])
 OPPOSITE_MOVES = {\
     Move.W : set([Move.E]),\
     Move.E : set([Move.W]),\
@@ -103,52 +103,61 @@ class Board:
                 rows_deleted += 1
         return rows_deleted
 
-    def __check_move_set(self, move_set, move):
+    def __check_move_chain(self, move_chain, move):
         if move not in VERTICAL_MOVE_ACTION:
-            return OPPOSITE_MOVES[move] not in move_set
+            if OPPOSITE_MOVES[move] not in move_chain:
+                if len(move_chain) > 1:
+                    is_rmr = move_chain[0] in ROTATION_ACTION
+                    if is_rmr:
+                        return move not in ROTATION_ACTION
+                    else:
+                        return move not in HORIZONTAL_MOVE_ACTION
+            else:
+                return False
         return True
 
     def get_valid_final_states(self, unit):
-        #import pdb
-        #pdb.set_trace()
         all_paths = []
-        tree = {'action': -1, 'move_set': set(), 'rot_count': 0, 'path': []}
+        tree = {'action': -1, 'move_chain': [], 'rot_count': 0, 'path': [], 'unit': deepcopy(unit), 'orientation': 0}
         working_list = [{'parent' : tree}]
         while len(working_list) > 0:
-            curt = working_list.pop()
-            allowed_moves = POSSIBLE_MOVES - (OPPOSITE_MOVES[curt['parent']['action']])
+            __curt = working_list.pop()
+            allowed_moves = POSSIBLE_MOVES - (OPPOSITE_MOVES[__curt['parent']['action']])
             for action in allowed_moves:
+                curt = {}
                 curt['action'] = action
-                curt['move_set'] = deepcopy(curt['parent']['move_set'])
-                curt['rot_count'] = curt['parent']['rot_count']
-                if (action in ROTATION_ACTION) and (action == curt['parent']['action']):
-                    if (curt['rot_count'] < 3):
-                        curt['rot_count'] += 1
-                    else:
-                        curt['unit'] = None
-                        print("Fail rot")
-                        continue #3 due > 180 degrees rotation
+                curt['move_chain'] = deepcopy(__curt['parent']['move_chain'])
+                curt['rot_count'] = __curt['parent']['rot_count']
+                curt['orientation'] = __curt['parent']['orientation']
+                if (action in ROTATION_ACTION):
+                    curt['rot_count'] += 1
+                    curt['orientation'] += 1 if action == Rotate.CW else -1
+                    if (action == __curt['parent']['action']):
+                        if (curt['rot_count'] > 3):
+                            print("Fail rot")
+                            continue #3 due > 180 degrees rotation
                 else:
                     curt['rot_count'] = 0
 
-                if not self.__check_move_set(curt['move_set'], action):
-                    curt['unit'] = None
+                if not self.__check_move_chain(curt['move_chain'], action):
                     print("Fail chain")
                     continue #chain of (r+m+r+)* or (m+r+m+)* causes infinite cycle
-                elif action in VERTICAL_MOVE_ACTION:
-                    curt['move_set'] = set()
+                
+                if action in VERTICAL_MOVE_ACTION:
+                    curt['move_chain'] = []
 
-                newu = deepcopy(unit)
+                newu = deepcopy(__curt['parent']['unit'])
                 newu.move(action)
-                if not self.is_locked(np.array(unit.cells)):
+                if not self.is_locked(np.array(newu.cells)):
                     curt['unit'] = newu
-                    curt['path'] = deepcopy(curt['parent']['path']) + [action]
-                    curt['move_set'].union(set([action]))
+                    curt['path'] = deepcopy(__curt['parent']['path']) + [action]
+                    if action not in curt['move_chain']:
+                        curt['move_chain'] += [action]
                     working_list += [{'parent': curt}]
                     print("Added")
-                    #res = self.__get_all_final_states(newu, path + [action], action, new_rot_count, move_set.union(set([action])))
+                    #res = self.__get_all_final_states(newu, path + [action], action, new_rot_count, move_chain.union(set([action])))
                     #all_paths += res
                 else:
                     print("Locked")
-                    all_paths += [(path, unit)]
+                    all_paths += [(deepcopy(__curt['parent']['path']), deepcopy(unit), __curt['parent']['orientation'])]
         return all_paths
